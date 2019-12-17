c3e436272ba246ba3551cb78917f837b9f45ad7a
simongarisch <gatman946@gmail.com>
2019-12-18 05:46:15 +1100

    working on goroutines

---
+++ goroutines.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func returnType(url string) {
+	resp, err := http.GEt(url)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("content-type")
+	fmt.Printf("%s -> %s\n", url, ctype)
+}
+
+func main() {
+	urls := []string{
+		"https://golang.org",
+		"https://api.github.com",
+		"https://httpbin.org.xml",
+	}
+}
