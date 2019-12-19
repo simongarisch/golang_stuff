b1a4d091247e893de5b62ae096f2ddf6bedd28e0
simongarisch <gatman946@gmail.com>
2019-12-20 09:13:51 +1100

    adding to slices

--- slices.go
+++ slices.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// arrays have a fixed size
+// but we can change the size of slices
+
 func main() {
 	// a slice is a sequence of items
 	loons := []string{"bugs", "daffy", "taz"}
@@ -26,4 +29,17 @@ func main() {
 		fmt.Printf("%s at %d\n", name, index)
 	}
 	fmt.Println("----")
+
+	a1 := [3]string{"a", "b", "c"}
+	sli1 := a1[0:1]
+	fmt.Println(len(sli1), cap(sli1)) // 1, 3 -> length and capacity
+	fmt.Println("----")
+
+	// variable slices
+	sli := make([]int, 10)
+	fmt.Println(sli)                // [0 0 0 0 0 0 0 0 0 0]
+	fmt.Println(len(sli), cap(sli)) // 10, 10
+
+	// there is a time penalty if you append to a slice beyond allocated capacity
+	// as a new slice has to be created and returned
 }
