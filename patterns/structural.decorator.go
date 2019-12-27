d3c92fd83bab0cf32221410aa4a42c78a98007d0
simongarisch <gatman946@gmail.com>
2019-12-28 05:36:43 +1100

    adding decorator pattern

---
+++ patterns/structural.decorator.go
@@ -0,0 +1,28 @@
+package main
+
+import "log"
+
+type Object func(int) int
+
+func LogDecorate(fn Object) Object {
+	return func(n int) int {
+		log.Println("Starting the execution with the integer", n)
+
+		result := fn(n)
+
+		log.Println("Execution is completed with the result", result)
+
+		return result
+	}
+}
+
+func Double(n int) int {
+	return n * 2
+}
+
+func main() {
+	f := LogDecorate(Double)
+	f(5)
+	// 2019/12/28 05:36:01 Starting the execution with the integer 5
+	// 2019/12/28 05:36:01 Execution is completed with the result 10
+}
