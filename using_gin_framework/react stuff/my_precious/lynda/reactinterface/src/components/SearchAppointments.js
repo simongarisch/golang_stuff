81a02b256ef817402b4c51e4cd9cbae75960358f
simongarisch <gatman946@gmail.com>
2020-01-25 19:21:51 +1100

    working on tut

---
+++ using_gin_framework/react stuff/my_precious/lynda/reactinterface/src/components/SearchAppointments.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+export default class SearchAppointments extends Component {
+    render() {
+        return (
+            <div>Search Appointments</div>
+        )
+    }
+}
