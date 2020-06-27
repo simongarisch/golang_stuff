fa5a0b1592be6fc3fc8304aa4be3ebfe1dd6aa52
simongarisch <gatman946@gmail.com>
2020-06-27 10:41:36 +1000

    adding adaptor pattern

---
+++ patterns/go_design_patterns/structural/adaptor/adaptor.go
@@ -0,0 +1,42 @@
+/*
+The Adapter pattern is very useful when, for example, an interface gets outdated and it's
+not possible to replace it easily or fast. Instead, you create a new interface to deal with the
+current needs of your application, which, under the hood, uses implementations of the old
+interface.
+*/
+
+package main
+
+import "fmt"
+
+type LegacyPrinter interface {
+	Print(s string) string
+}
+
+type MyLegacyPrinter struct{}
+
+func (l *MyLegacyPrinter) Print(s string) (newMsg string) {
+	newMsg = fmt.Sprintf("Legacy Printer: %s\n", s)
+	println(newMsg)
+	return newMsg
+}
+
+type NewPrinter interface {
+	PrintStored() string
+}
+
+type PrinterAdapter struct {
+	OldPrinter LegacyPrinter
+	Msg        string
+}
+
+func (p *PrinterAdapter) PrintStored() (newMsg string) {
+	if p.OldPrinter != nil {
+		newMsg = fmt.Sprintf("Adapter: %s", p.Msg)
+		newMsg = p.OldPrinter.Print(newMsg)
+	} else {
+		newMsg = p.Msg
+	}
+
+	return
+}
