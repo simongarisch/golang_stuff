0dad814f86753bf41b3a4f0441d4ce14bdece9a0
simongarisch <gatman946@gmail.com>
2020-07-17 08:28:15 +1000

    adding sync examples

---
+++ patterns/concingo/12_mutex.go
@@ -0,0 +1,52 @@
+/*
+You’ll notice that we always call Unlock within a defer statement.
+This is a very common idiom when utilizing a Mutex to ensure the call always happens, even when panicing.
+Failing to do so will probably cause your program to deadlock.
+*/
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var count int
+	var lock sync.Mutex
+
+	increment := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count++
+		fmt.Printf("Incrementing: %d\n", count)
+	}
+
+	decrement := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count--
+		fmt.Printf("Decrementing: %d\n", count)
+	}
+
+	// Increment
+	var arithmetic sync.WaitGroup
+	for i := 0; i <= 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			increment()
+		}()
+	}
+
+	// Decrement
+	for i := 0; i <= 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			decrement()
+		}()
+	}
+
+	arithmetic.Wait()
+	fmt.Println("Arithmetic complete.")
+}
