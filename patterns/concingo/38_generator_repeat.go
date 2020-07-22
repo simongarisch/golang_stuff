9fcea53707399c8f8b3e45660857058c2b507cbb
simongarisch <gatman946@gmail.com>
2020-07-22 11:39:07 +1000

    adding generator and fan out examples

---
+++ patterns/concingo/38_generator_repeat.go
@@ -0,0 +1,57 @@
+// go run 38_generator_repeat.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// This function will repeat the values you pass to it infinitely until you tell it to stop.
+	repeat := func(
+		done <-chan interface{},
+		values ...interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	// This pipeline stage will only take the first num items off of its incoming valueStream and then exit.
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	for num := range take(done, repeat(done, 1), 10) {
+		fmt.Printf("%v ", num)
+	}
+}
