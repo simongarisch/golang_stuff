755255a7e2edfb08de0ffdb8ecac0dea40336cef
simongarisch <gatman946@gmail.com>
2020-10-23 08:38:40 +1100

    adding anko example

---
+++ anko_run.go
@@ -0,0 +1,37 @@
+// go run anko_run.go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/mattn/anko/env"
+	"github.com/mattn/anko/vm"
+)
+
+func main() {
+	e := env.NewEnv()
+
+	err := e.Define("println", fmt.Println)
+	if err != nil {
+		log.Fatalf("Define error: %v\n", err)
+	}
+
+	script := `
+println("Hello World :)")
+
+a = map[interface]interface{"x": 1}
+println(a) // map[x:1]
+a.b = 2
+a["c"] = 3
+println(a["b"]) // 2
+println(a.c) // 3
+`
+
+	_, err = vm.Execute(e, nil, script)
+	if err != nil {
+		log.Fatalf("Execute error: %v\n", err)
+	}
+
+	// output: Hello World :)
+}
