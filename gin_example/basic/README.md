876270ade1c4f1eb8dd51e89f55d2f45d29bcbf7
simongarisch <gatman946@gmail.com>
2020-11-08 14:30:45 +1100

    basic gin example

---
+++ gin_example/basic/README.md
@@ -0,0 +1,3 @@
+
+https://semaphoreci.com/community/tutorials/building-go-web-applications-and-microservices-using-gin
+https://chenyitian.gitbooks.io/gin-tutorials/content/
---
+++ gin_example/basic/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initialiseRoutes() {
+	// Handle the index route
+	router.GET("/", func(c *gin.Context) {
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+
+	})
+}
