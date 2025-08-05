68f1c361cc0c53534c27fe6bbf577ed1a83ba292
simongarisch <31283453+simongarisch@users.noreply.github.com>
2025-08-05 07:31:00 +0000

    working in handlers

--- testing_in_go/webapp/cmd/web/handlers.go
+++ testing_in_go/webapp/cmd/web/handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"path"
 )
+
+var pathToTemplates = "./templates/"
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "home.html", &TemplateData{})
@@ -16,7 +19,7 @@ type TemplateData struct {
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
 	// parse the template from disk
-	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
 	if err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return err
